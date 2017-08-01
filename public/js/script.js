6a19ff2853233824e0e8202cad69495c76c6d948
Camille Sébastien Niessen <camille.s.niessen@icloud.com>
2017-08-01 16:55:23 +0200

    First changes

--- public/js/script.js
+++ public/js/script.js
@@ -5,15 +5,24 @@
     info: document.querySelector('.__info'),
     close: document.querySelector('#close'),
     title: document.querySelector('.__title'),
+    copyright: document.querySelector('#copy_date'),
+    image: document.querySelectorAll('.__clickable'),
   }
 
   let imageInterval = false;
+  let negativeInterval = false;
   let actualNumber = false;
 
   const app = {
     init() {
       images.init();
       eventListeners.init();
+      this.setCopyright();
+    },
+    setCopyright() {
+      const date = new Date();
+      const year = date.getFullYear();
+      elements.copyright.textContent = year;
     }
   }
 
@@ -26,10 +35,21 @@
       images.startInterval(number)
     },
     startInterval() {
-      imageInterval = setInterval(images.changeImage, 7500);
+      imageInterval = setInterval(images.changeImage, 6000);
     },
     pauzeInterval() {
       clearInterval(imageInterval);
+    },
+    negativeInterval() {
+      const amountOfGroups = elements.image_groups.length - 1;
+      const number = Math.floor(Math.random() * amountOfGroups) + 1;
+      elements.image_groups.forEach(function(group) {
+        group.style.opacity = 0;
+      });
+      elements.image_groups[number].childNodes[1].style.opacity = 0;
+      elements.image_groups[number].childNodes[3].style.opacity = 1;
+      elements.image_groups[number].style.opacity = 1;
+      actualNumber = number;
     },
     changeImage() {
       const amountOfGroups = elements.image_groups.length - 1;
@@ -37,6 +57,7 @@
       elements.image_groups.forEach(function(group) {
         group.style.opacity = 0;
       });
+      elements.image_groups[number].childNodes[1].style.opacity = 1;
       elements.image_groups[number].style.opacity = 1;
       actualNumber = number;
     },
@@ -46,9 +67,13 @@
     init() {
       elements.title.addEventListener("click", eventListeners.openInfo);
       elements.close.addEventListener("click", eventListeners.closeInfo);
+      elements.image.forEach(function(image){
+        image.addEventListener("click", eventListeners.goToNext);
+      })
     },
     openInfo() {
       clearInterval(imageInterval);
+      negativeInterval = setInterval(images.negativeInterval, 6000);
       elements.image_groups[actualNumber].childNodes[1].style.opacity = 0;
       elements.image_groups[actualNumber].childNodes[3].style.opacity = 1;
       elements.info.style.opacity = 1;
@@ -56,13 +81,18 @@
       document.body.style.backgroundColor = 'white';
     },
     closeInfo() {
-      imageInterval = setInterval(images.changeImage, 7500);
+      clearInterval(negativeInterval);
+      imageInterval = setInterval(images.changeImage, 6000);
       elements.image_groups[actualNumber].childNodes[1].style.opacity = 1;
       elements.image_groups[actualNumber].childNodes[3].style.opacity = 0;
       elements.info.style.opacity = 0;
       elements.info.style.zIndex = -100000;
       document.body.style.backgroundColor = 'black';
-    }
+    },
+    goToNext() {
+      // LOGIC HERE
+      console.log('click');
+    },
   }
 
   app.init();
