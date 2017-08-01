48672fe6c279759f0ad45f39c3b810e3bcfe1aca
Camille Sébastien Niessen <camille.s.niessen@icloud.com>
2017-08-01 12:50:37 +0200

    Fade effects + new images

--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,60 @@
 (function () {
 
+  const elements = {
+    image_groups: document.querySelectorAll('.__image_wrapper'),
+    title: document.querySelector('.__title'),
+  }
+
+  let imageInterval = false;
+  let actualNumber = false;
+
+  const app = {
+    init() {
+      images.init();
+      eventListeners.init();
+    },
+    enhance() {
+
+    }
+  }
+
+  const images = {
+    init() {
+      const amountOfGroups = elements.image_groups.length - 1;
+      const number = Math.floor(Math.random() * amountOfGroups) + 1;
+      elements.image_groups[number].style.opacity = 1;
+      actualNumber = number;
+      images.startInterval(number)
+    },
+    startInterval() {
+      imageInterval = setInterval(images.changeImage, 7500);
+    },
+    pauzeInterval() {
+      clearInterval(imageInterval);
+    },
+    changeImage() {
+      const amountOfGroups = elements.image_groups.length - 1;
+      const number = Math.floor(Math.random() * amountOfGroups) + 1;
+      elements.image_groups.forEach(function(group) {
+        group.style.opacity = 0;
+      });
+      elements.image_groups[number].style.opacity = 1;
+      actualNumber = number;
+    },
+  }
+
+  const eventListeners = {
+    init() {
+      elements.title.addEventListener("click", eventListeners.openInfo);
+    },
+    openInfo() {
+      clearInterval(imageInterval);
+      elements.image_groups[actualNumber].childNodes[1].style.opacity = 0;
+      elements.image_groups[actualNumber].childNodes[3].style.opacity = 1;
+      console.log(actualNumber);
+    }
+  }
+
+  app.init();
+
 })();
