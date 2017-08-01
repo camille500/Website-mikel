42f6e1d9ebeb2399432737b291ad7fbbb8ba215c
Camille Sébastien Niessen <camille.s.niessen@icloud.com>
2017-08-01 10:59:18 +0200

    Auto insert images into webpage

--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,20 @@ const express = require('express');
 const router = express.Router();
 const path = require('path');
 const bodyParser = require('body-parser');
+const fs = require('fs');
 
 /* INDEX ROUTE
 ----------------------------------------- */
 router.get('/', function(req, res) {
-  res.render('/index');
+  req.session.images = [];
+  const imageFolder = 'public/images'
+  fs.readdir(imageFolder, (err, files) => {
+    files.forEach(file => {
+      req.session.images.push(file);
+    });
+    res.locals.images = req.session.images;
+    res.render('index');
+  });
 });
 
 /* EXPORT ROUTER
