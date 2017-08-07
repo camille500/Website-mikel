a97063b4e46a0caac967ca1e22510e63b782bcce
Camille Sébastien Niessen <camille.s.niessen@icloud.com>
2017-08-07 09:29:15 +0200

    admin route

--- routes/admin.js
+++ routes/admin.js
@@ -30,6 +30,20 @@ router.post('/', function(req, res) {
   }
 });
 
+router.get('/overview', function(req, res) {
+  req.session.images = [];
+  const imageFolder = 'public/dist/images'
+  fs.readdir(imageFolder, (err, files) => {
+    files.forEach(file => {
+      if(file.charAt(3) == 1) {
+        req.session.images.push(file);
+      }
+    });
+    res.locals.images = req.session.images;
+    res.render('admin/overview');
+  });
+})
+
 router.get('/edit/:image', function(req, res) {
   res.locals.image = req.params.image
   res.render('admin/edit')
@@ -41,9 +55,8 @@ router.get('/delete/:image', function(req, res) {
   const imagePaths = [`public/dist/images/${image}`, `public/dist/images/${negative}`]
   imagePaths.forEach(function(image) {
     fs.unlink(image, function(error) {
-    if (error) { throw error; }
-     
     });
+    res.redirect('/admin/overview');
   })
 })
 
