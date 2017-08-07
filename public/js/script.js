52b1def676d624f046626f18a588f596864356c8
Camille Sébastien Niessen <camille.s.niessen@icloud.com>
2017-08-07 07:42:26 +0200

    All fixed

--- public/js/script.js
+++ public/js/script.js
@@ -5,24 +5,27 @@
     image: document.querySelectorAll('.__image'),
     info: document.querySelector('.__info'),
     close: document.querySelector('#close'),
-    title: document.querySelector('.__title'),
+    title: document.querySelectorAll('.__title'),
     copyright: document.querySelector('#copy_date'),
     image: document.querySelectorAll('.__clickable'),
-    about: document.querySelector('.__about_title'),
+    title_one: document.querySelector('.one'),
+    title_two: document.querySelector('.two'),
+    title_three: document.querySelector('.three'),
   }
 
   let imageInterval = false;
   let negativeInterval = false;
   let actualNumber = false;
-  latestNumber = false;
   let allLatest = [];
+  let latestTitle = false;
 
   const app = {
     init() {
       images.init();
       eventListeners.init();
       this.setCopyright();
-      titleInterval = setInterval(app.changeTitle, 5000);
+      app.changeTitle();
+      titleInterval = setInterval(app.changeTitle, 3000);
     },
     setCopyright() {
       const date = new Date();
@@ -30,30 +33,44 @@
       elements.copyright.textContent = year;
     },
     changeTitle() {
-      const text = elements.about.textContent;
-      if(text === 'Contact') {
-        elements.about.textContent = 'About';
+      if(latestTitle === false) {
+        latestTitle = 'one';
+        elements.title_three.style.opacity = 0;
+        elements.title_one.style.opacity = 1;
+      } else if(latestTitle === 'one') {
+        latestTitle = 'two';
+        elements.title_one.style.opacity = 0;
+        setTimeout(function() {
+          elements.title_two.style.opacity = 1;
+        }, 1000)
       } else {
-        elements.about.textContent = 'Contact';
+        latestTitle = false;
+        elements.title_two.style.opacity = 0;
+        setTimeout(function() {
+          elements.title_three.style.opacity = 1;
+        }, 1000)
       }
     }
   }
 
   const images = {
     init() {
-      const number = this.generateNumber();
-      this.changeImage();
+      const amountOfGroups = elements.image_groups.length - 1;
+      let number = Math.floor(Math.random() * amountOfGroups) + 1;
       actualNumber = number;
-      images.startInterval(number)
+      this.changeImage();
+      images.startInterval()
     },
     startInterval() {
-      imageInterval = setInterval(images.changeImage, 4000);
+      imageInterval = setInterval(images.changeImage, 5000);
     },
     pauzeInterval() {
       clearInterval(imageInterval);
     },
     negativeInterval() {
-      const number = images.generateNumber();
+      const amountOfGroups = elements.image_groups.length - 1;
+      let number = Math.floor(Math.random() * amountOfGroups) + 1;
+      actualNumber = number;
       elements.image_groups.forEach(function(group) {
         group.style.opacity = 0;
       });
@@ -62,47 +79,22 @@
       elements.image_groups[number].style.opacity = 1;
     },
     changeImage() {
-      const number = images.generateNumber();
+      const amountOfGroups = elements.image_groups.length - 1;
+      let number = Math.floor(Math.random() * amountOfGroups) + 1;
+      actualNumber = number;
       elements.image_groups.forEach(function(group) {
         group.style.opacity = 0;
       });
       elements.image_groups[number].childNodes[1].style.opacity = 1;
       elements.image_groups[number].style.opacity = 1;
     },
-    generateNumber() {
-      const amountOfGroups = elements.image_groups.length - 1;
-      let number = Math.floor(Math.random() * amountOfGroups) + 1;
-      latestNumber = number;
-      if(allLatest.length <= amountOfGroups) {
-        if(allLatest.indexOf(number) == -1) {
-          allLatest.push(number);
-          actualNumber = number;
-        } else {
-          number = images.newNumber();
-          allLatest.push(number);
-          actualNumber = number;
-        }
-      } else {
-        allLatest = [];
-      }
-      return number;
-    },
-    newNumber() {
-      const amountOfGroups = elements.image_groups.length - 1;
-      const number = Math.floor(Math.random() * amountOfGroups) + 1;
-      if(allLatest.indexOf(number) == -1) {
-        return number;
-      } else {
-        const number = Math.floor(Math.random() * amountOfGroups) + 1;
-        return number;
-      }
-    }
   }
 
   const eventListeners = {
     init() {
-      elements.title.addEventListener("click", eventListeners.openInfo);
-      elements.about.addEventListener("click", eventListeners.openInfo);
+      elements.title.forEach(function(title){
+        title.addEventListener("click", eventListeners.openInfo);
+      })
       elements.close.addEventListener("click", eventListeners.closeInfo);
       elements.image.forEach(function(image){
         image.addEventListener("click", eventListeners.goToNext);
@@ -110,47 +102,30 @@
     },
     openInfo() {
       clearInterval(imageInterval);
-<<<<<<< HEAD
       negativeInterval = setInterval(images.negativeInterval, 5000);
-      console.log(actualNumber)
-      elements.image_groups[latestNumber].childNodes[1].style.display = 'none';
-      elements.image_groups[latestNumber].childNodes[3].style.opacity = 1;
-      elements.about.style.display = 'none';
-      elements.title.style.display = 'none';
-=======
-      negativeInterval = setInterval(images.negativeInterval, 4000);
       elements.image_groups[actualNumber].childNodes[1].style.opacity = 0;
       elements.image_groups[actualNumber].childNodes[3].style.opacity = 1;
->>>>>>> parent of 182dac2... Test
+      elements.title.forEach(function(title) {
+        title.style.opacity = 0;
+      })
       elements.info.style.opacity = 1;
       elements.info.style.zIndex = 100000;
-      // document.body.style.backgroundColor = 'white';
+      document.body.style.backgroundColor = 'white';
     },
     closeInfo() {
       clearInterval(negativeInterval);
-<<<<<<< HEAD
       imageInterval = setInterval(images.changeImage, 5000);
-      elements.image_groups[latestNumber].childNodes[1].style.opacity = 1;
-=======
-      imageInterval = setInterval(images.changeImage, 4000);
       elements.image_groups[actualNumber].childNodes[1].style.opacity = 1;
->>>>>>> parent of 182dac2... Test
       elements.image_groups[actualNumber].childNodes[3].style.opacity = 0;
       elements.info.style.opacity = 0;
       elements.info.style.zIndex = -100000;
       document.body.style.backgroundColor = 'black';
-      elements.about.style.display = 'block';
       elements.title.style.display = 'block';
     },
     goToNext() {
       clearInterval(imageInterval);
       images.changeImage();
-<<<<<<< HEAD
       imageInterval = setInterval(images.changeImage, 5000);
-=======
-      imageInterval = setInterval(images.changeImage, 4000);
-      console.log('click');
->>>>>>> parent of 182dac2... Test
     },
   }
 
