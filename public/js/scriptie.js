e7d395204cadee565bc6e9fa22ee33f41b9f793f
Camille Sébastien Niessen <camille.s.niessen@icloud.com>
2017-09-01 11:29:50 +0200

    no message

--- public/js/scriptie.js
+++ public/js/scriptie.js
@@ -1,170 +1,172 @@
-/* ELEMENTEN */
-var image_groups = document.getElementsByClassName('__image_wrapper'),
-    image_holders = document.getElementsByClassName('__cross-fade'),
-    image = document.getElementsByClassName('__image'),
-    copyright = document.getElementById('copy_date'),
-    title_one = document.getElementsByClassName('one')[0],
-    title_two = document.getElementsByClassName('two')[0],
-    title_three = document.getElementsByClassName('three')[0],
-    image_info = document.getElementsByClassName('__image_info'),
-    info = document.getElementsByClassName('__info')[0],
-    close = document.getElementById('close'),
-    title = document.getElementsByClassName('__title'),
-    description = document.getElementsByClassName('__description')[0],
-    rotate = document.getElementsByClassName('__title_rotate')[0],
-    blink_mail = document.getElementsByClassName('blink_mail'),
-    negative = document.getElementsByClassName('__negative'),
-    safari = document.getElementsByClassName('__safari_layer')[0];
+// /* ELEMENTEN */
+// var image_groups = document.getElementsByClassName('__image_wrapper'),
+//     image_holders = document.getElementsByClassName('__cross-fade'),
+//     image = document.getElementsByClassName('__image'),
+//     copyright = document.getElementById('copy_date'),
+//     title_one = document.getElementsByClassName('one')[0],
+//     title_two = document.getElementsByClassName('two')[0],
+//     title_three = document.getElementsByClassName('three')[0],
+//     image_info = document.getElementsByClassName('__image_info'),
+//     info = document.getElementsByClassName('__info')[0],
+//     close = document.getElementById('close'),
+//     title = document.getElementsByClassName('__title'),
+//     description = document.getElementsByClassName('__description')[0],
+//     rotate = document.getElementsByClassName('__title_rotate')[0],
+//     blink_mail = document.getElementsByClassName('blink_mail'),
+//     negative = document.getElementsByClassName('__negative'),
+//     safari = document.getElementsByClassName('__safari_layer')[0];
+//
+// var config = {
+//   allData: {},
+//   imageArray: [],
+//   atImage: 0,
+//   actualImage: 0,
+//   interval: 6000,
+//   imageUrl: '/dist/images/',
+//   replaceCount: 0,
+//   actualShown: false,
+//   safari_check: false,
+//   timeOut: 0
+// };
+//
+// var imageInterval = false;
+//
+// function initApplication() {
+//   console.log(info)
+//   getData();
+//   // initializeEventListeners();
+//   // initializeLayout();
+//   checkForSafari();
+//   setTimeout(function() {
+//     info.style.opacity = 1;
+//   }, 50)
+// }
+//
+// function checkForSafari() {
+//   var ua = navigator.userAgent.toLowerCase();
+//    if (ua.indexOf('safari') != -1) {
+//      if (ua.indexOf('chrome') > -1) {
+//        config.timeOut = 0;
+//      } else {
+//        console.log(ua.indexOf('mobile'))
+//        if(ua.indexOf('mobile') != -1) {
+//          elements.safari.style.display = 'none';
+//        } else {
+//          elements.safari.style.display = 'block';
+//          config.timeOut = 1200;
+//        }
+//      }
+//    }
+// }
+//
+// function getData() {
+//   var request = new XMLHttpRequest();
+//   request.onreadystatechange = function() {
+//     if (this.readyState == 4 && this.status == 200) {
+//       var data = JSON.parse(request.responseText);
+//       config.allData = data;
+//     }
+//     makeDataArray();
+//   };
+//   request.open("GET", "http://studio-orphee.eu/images", true);
+//   request.send();
+// }
+//
+// function makeDataArray() {
+//   for (var key in config.allData.images) {
+//     config.imageArray.push(key);
+//   }
+//   shuffleDataArray(config.imageArray);
+// }
+//
+// function shuffleDataArray(array) {
+//   array.forEach(function(image, index) {
+//     var j = Math.floor(Math.random() * index);
+//     var _ref = [array[j], array[index]];
+//     array[index] = _ref[0];
+//     array[j] = _ref[1];
+//     _ref;
+//   });
+//   if (array.length) {
+//     console.log(array)
+//     config.imageArray = array;
+//     initializeImages();
+//   }
+// }
+//
+// function initializeImages() {
+//   var imageArray = config.imageArray;
+//   for(var i = 0; i < image_holders.length; i++) {
+//     image_holders[i].src = config.imageUrl + imageArray[i];
+//     config.atImage ++;
+//   }
+//   for(var i = 0; i < negative.length; i++) {
+//     var source = imageArray[i].replace('.1', '.2');
+//     negative[i].src = config.imageUrl + source;
+//   }
+//   var source = image_groups[0].childNodes[1].src;
+//   var source_length = source.length;
+//   var description = source.substr(source_length - 8, source_length);
+//   description.textContent = config.allData.images[description];
+//   config.imageArray = imageArray;
+//   startImageInterval();
+// }
+//
+// function startImageInterval() {
+//   setTimeout(function() {
+//     image_groups[0].style.opacity = 1;
+//     safari.style.display = 'none';
+//   }, config.timeOut);
+//   for(var i = 0; i < image.length; i++) {
+//     image[i].style.opacity = 1;
+//   }
+//   clearInterval(imageInterval);
+//   imageInterval = setInterval(crossFadeImages, config.interval);
+// }
+//
+// function crossFadeImages() {
+//   var actual = config.actualImage;
+//   var next = actual + 1;
+//   if(config.actualImage === 0) {
+//     changeRestOfImages();
+//   }
+//   if(config.atImage >= (config.imageArray.length - 1)) {
+//     clearInterval(imageInterval);
+//     config.atImage = 1;
+//     initializeImages();
+//   }
+//   config.actualImage ++;
+//   if(config.actualImage > 4) {
+//     config.actualImage = 0;
+//     image_groups[actual].style.opacity = 0;
+//     image_groups[config.actualImage].style.opacity = 1;
+//     var source = image_groups[config.actualImage].childNodes[1].src
+//     var source_length = source.length;
+//     var description = source.substr(source_length - 8, source_length);
+//     description.textContent = config.allData.images[description];
+//   } else {
+//     if(config.actualImage > 2) {
+//       changeFirstThreeImages();
+//     }
+//     image_groups[actual].style.opacity = 0;
+//     image_groups[next].style.opacity = 1;
+//     var source = image_groups[config.actualImage].childNodes[1].src
+//     var source_length = source.length;
+//     var description = source.substr(source_length - 8, source_length);
+//     description.textContent = config.allData.images[description];
+//   }
+// }
+//
+// function changeFirstThreeImages() {
+//   // KOMT NOG
+// }
+//
+// function changeRestOfImages() {
+//   // KOMT NOG
+// }
+//
+//
+//
+// initApplication();
 
-var config = {
-  allData: {},
-  imageArray: [],
-  atImage: 0,
-  actualImage: 0,
-  interval: 6000,
-  imageUrl: '/dist/images/',
-  replaceCount: 0,
-  actualShown: false,
-  safari_check: false,
-  timeOut: 0
-};
-
-var imageInterval = false;
-
-function initApplication() {
-  console.log(info)
-  getData();
-  // initializeEventListeners();
-  // initializeLayout();
-  checkForSafari();
-  setTimeout(function() {
-    info.style.opacity = 1;
-  }, 50)
-}
-
-function checkForSafari() {
-  var ua = navigator.userAgent.toLowerCase();
-   if (ua.indexOf('safari') != -1) {
-     if (ua.indexOf('chrome') > -1) {
-       config.timeOut = 0;
-     } else {
-       console.log(ua.indexOf('mobile'))
-       if(ua.indexOf('mobile') != -1) {
-         elements.safari.style.display = 'none';
-       } else {
-         elements.safari.style.display = 'block';
-         config.timeOut = 1200;
-       }
-     }
-   }
-}
-
-function getData() {
-  var request = new XMLHttpRequest();
-  request.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      var data = JSON.parse(request.responseText);
-      config.allData = data;
-    }
-    makeDataArray();
-  };
-  request.open("GET", "http://studio-orphee.eu/images", true);
-  request.send();
-}
-
-function makeDataArray() {
-  for (var key in config.allData.images) {
-    config.imageArray.push(key);
-  }
-  shuffleDataArray(config.imageArray);
-}
-
-function shuffleDataArray(array) {
-  array.forEach(function(image, index) {
-    var j = Math.floor(Math.random() * index);
-    var _ref = [array[j], array[index]];
-    array[index] = _ref[0];
-    array[j] = _ref[1];
-    _ref;
-  });
-  if (array.length) {
-    console.log(array)
-    config.imageArray = array;
-    initializeImages();
-  }
-}
-
-function initializeImages() {
-  var imageArray = config.imageArray;
-  for(var i = 0; i < image_holders.length; i++) {
-    image_holders[i].src = config.imageUrl + imageArray[i];
-    config.atImage ++;
-  }
-  for(var i = 0; i < negative.length; i++) {
-    var source = imageArray[i].replace('.1', '.2');
-    negative[i].src = config.imageUrl + source;
-  }
-  var source = image_groups[0].childNodes[1].src;
-  var source_length = source.length;
-  var description = source.substr(source_length - 8, source_length);
-  description.textContent = config.allData.images[description];
-  config.imageArray = imageArray;
-  startImageInterval();
-}
-
-function startImageInterval() {
-  setTimeout(function() {
-    image_groups[0].style.opacity = 1;
-    safari.style.display = 'none';
-  }, config.timeOut);
-  for(var i = 0; i < image.length; i++) {
-    image[i].style.opacity = 1;
-  }
-  clearInterval(imageInterval);
-  imageInterval = setInterval(crossFadeImages, config.interval);
-}
-
-function crossFadeImages() {
-  var actual = config.actualImage;
-  var next = actual + 1;
-  if(config.actualImage === 0) {
-    changeRestOfImages();
-  }
-  if(config.atImage >= (config.imageArray.length - 1)) {
-    clearInterval(imageInterval);
-    config.atImage = 1;
-    initializeImages();
-  }
-  config.actualImage ++;
-  if(config.actualImage > 4) {
-    config.actualImage = 0;
-    image_groups[actual].style.opacity = 0;
-    image_groups[config.actualImage].style.opacity = 1;
-    var source = image_groups[config.actualImage].childNodes[1].src
-    var source_length = source.length;
-    var description = source.substr(source_length - 8, source_length);
-    description.textContent = config.allData.images[description];
-  } else {
-    if(config.actualImage > 2) {
-      changeFirstThreeImages();
-    }
-    image_groups[actual].style.opacity = 0;
-    image_groups[next].style.opacity = 1;
-    var source = image_groups[config.actualImage].childNodes[1].src
-    var source_length = source.length;
-    var description = source.substr(source_length - 8, source_length);
-    description.textContent = config.allData.images[description];
-  }
-}
-
-function changeFirstThreeImages() {
-  // KOMT NOG
-}
-
-function changeRestOfImages() {
-  // KOMT NOG
-}
-
-
-
-initApplication();
+alert('Jolo')
